b8713c0260997e3c856c442fcb7517cd6b99662e
github username <github email address>
2021-03-05 09:22:21 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 
 window.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('form').addEventListener('submit', addUser)
+    getRepos()
+    // document.querySelector("#user-info").addEventListener('click', showUser)
+    document.querySelector('form').addEventListener('submit', setUser)
   })
+
+  let user = 'curlywallst'
+  const BASE_URL = 'https://api.github.com/'
   
-  function addUser(event){
-    event.preventDefault()
-    let username = document.getElementById('username').value
-    let age = document.getElementById('age').value
-    let hobby = document.getElementById('hobby').value
-  
-    let user = {
-      name: username,
-      age: age,
-      hobby: hobby
-    }
-  
-    console.log(user)
+  function getRepos() {
+    console.log("get repos")
+    document.getElementById('main').innerHTML=""
+    // let button = document.querySelector("#repos")
+    // button.parentElement.removeChild(button)
+    main.innerHTML=`<button id="user-info">User Info</button>`   
+    document.querySelector("#user-info").addEventListener('click', showUser) 
     let list = document.getElementById('list')
-    list.innerHTML += (
-      `
-        <tr>
-          <td>${user.name}</td>
-          <td>${user.age}</td>
-          <td>${user.hobby}</td>
-        </tr>
-        ` )
-  
-    document.getElementById('username').value = ''
-    document.getElementById('age').value = ''
-    document.getElementById('hobby').value = ''
+    fetch(`${BASE_URL}users/${user}/repos`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      data.forEach( repo => {
+        list.innerHTML += (
+          `
+            <tr>
+              <td>${repo.name}</td>
+              <td>${repo.html_url}</td>
+            </tr>`)
+      })
+    })
   }
   
-  
+
+  function showUser() {
+    console.log("show user")
+    let list = document.getElementById('list')
+    list.innerHTML = ""
+    let main = document.getElementById('main')
+    fetch(`${BASE_URL}users/${user}`)
+    .then(res => res.json())
+    .then(user => {
+      console.log(user)
+      let button = document.querySelector("#user-info")
+      button.parentElement.removeChild(button)
+      main.innerHTML=`<button id="repos">Show Repos</button>`
+
+      main.innerHTML+=`
+        <p><strong>Username: </strong>${user.login}</p>
+        <p><strong>Name: </strong>${user.name}</p>
+        <p><strong>Bio: </strong>${user.bio}</p>
+        <p><strong>Location: </strong>${user.location}</p>
+      `
+      document.querySelector("#repos").addEventListener('click', getRepos)
+
+    })
+  }
+
+  function setUser(event){
+    event.preventDefault()
+    user = document.getElementById("username").value
+    document.getElementById("username").value = ""
+    getRepos()
+  }
